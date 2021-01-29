06b3326c403487080d6774139c092d4a5d5f4d49
Eimad Arnaout <earn1087@gmail.com>
2021-01-29 22:39:39 +1100

    finalised clear and decimal

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ const calculate = (n1, operator, n2) => {
 
     if (operator === 'add') {
         result = parseFloat(n1) + parseFloat(n2)
-      } else if (operator === 'subtract') {
+    } else if (operator === 'subtract') {
         result = parseFloat(n1) - parseFloat(n2)
-      } else if (operator === 'multiply') {
+    } else if (operator === 'multiply') {
         result = parseFloat(n1) * parseFloat(n2)
-      } else if (operator === 'divide') {
+    } else if (operator === 'divide') {
         result = parseFloat(n1) / parseFloat(n2)
-      }
+    }
 
     return result
 }
@@ -23,60 +23,93 @@ const calculate = (n1, operator, n2) => {
 //{for my reference, seems to work the same replacing above 2 lines}
 
 keys.addEventListener('click', e => {
-    if(e.target.matches('button')) {
+    if (e.target.matches('button')) {
 
-const key = e.target
-const action = key.dataset.action
-const keyContent = key.textContent
-const displayedNum = display.textContent
-const previousKeyType = calculator.dataset.previousKeyType
+        const key = e.target
+        const action = key.dataset.action
+        const keyContent = key.textContent
+        const displayedNum = display.textContent
+        const previousKeyType = calculator.dataset.previousKeyType
 
-// Remove .is-depressed class from all keys
-Array.from(key.parentNode.children)
-.forEach(k => k.classList.remove('is-depressed'))
+        // Remove .is-depressed class from all keys
+        Array.from(key.parentNode.children)
+            .forEach(k => k.classList.remove('is-depressed'))
 
-if(!action) {
-    if (displayedNum === '0' || previousKeyType === 'operator') {
-        display.textContent = keyContent
-      } else {
-        display.textContent = displayedNum + keyContent
-      }
-}
+        if (!action) {
 
-if (
-    action === 'add' ||
-    action === 'subtract' ||
-    action === 'multiply' ||
-    action === 'divide'
-) {
-    key.classList.add('is-depressed')
-    // Add custom attribute
-    calculator.dataset.previousKeyType = 'operator'
+            calculator.dataset.previousKeyType = 'number'
 
-    calculator.dataset.firstValue = displayedNum
-    calculator.dataset.operator = action
-}
+            if (displayedNum === '0' || previousKeyType === 'operator') {
+                display.textContent = keyContent
+            } else {
+                display.textContent = displayedNum + keyContent
+            }
+        }
 
-if (action === 'decimal') {
+        if (action !== 'clear') {
+            const clearButton = calculator.querySelector('[data-action=clear]')
+            clearButton.textContent = 'CE'
+        }
 
-    display.textContent = displayedNum + '.'
-}
+        if (action === 'clear') {
 
-if (action === 'clear') {
+            calculator.dataset.previousKeyType = 'clear'
 
-}
+            if (key.textContent === 'AC') {
+                calculator.dataset.firstValue = ''
+                calculator.dataset.modValue = ''
+                calculator.dataset.operator = ''
+                calculator.dataset.previousKeyType = ''
+            } else {
+                key.textContent = 'AC'
+            }
 
-if (action === 'calculate') {
-    const firstValue = calculator.dataset.firstValue
-    const operator = calculator.dataset.operator
-    const secondValue = displayedNum
+            display.textContent = 0
+            calculator.dataset.previousKeyType = 'clear'
 
-    display.textContent = calculate(firstValue, operator, 
-        secondValue)
+        }
 
-}
- 
-}
+        if (
+            action === 'add' ||
+            action === 'subtract' ||
+            action === 'multiply' ||
+            action === 'divide'
+        ) {
+            key.classList.add('is-depressed')
+            // Add custom attribute
+            calculator.dataset.previousKeyType = 'operator'
+
+            calculator.dataset.firstValue = displayedNum
+            calculator.dataset.operator = action
+        }
+
+        if (action === 'decimal') {
+
+            calculator.dataset.previousKeyType = 'decimal'
+
+            // Do nothing if string has a dot
+            if (!displayedNum.includes('.')) {
+                display.textContent = displayedNum + '.'
+            } else if (previousKeyType === 'operator') {
+                display.textContent = '0.'
+            }
+        }
+
+
+        if (action === 'calculate') {
+
+            calculator.dataset.previousKeyType = 'calculate'
+
+            const firstValue = calculator.dataset.firstValue
+            const operator = calculator.dataset.operator
+            const secondValue = displayedNum
+
+            display.textContent = calculate(firstValue, operator,
+                secondValue)
+
+        }
+
+    }
 })
 
 
